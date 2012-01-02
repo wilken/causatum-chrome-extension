277f712b1d0acfc511cb7185f4a0ccfb6c11a761
Morten Wilken <wilken@ultraplex.org>
2012-01-02 23:50:20 +0100

    fixed cross domain problem

--- js/content_script.js
+++ js/content_script.js
@@ -1,10 +1,25 @@
 var J = jQuery.noConflict();
-
+var text
+var url
 J(function(){
-	window.addEventListener("message", function(){J('#close').click()}, false);
+  chrome.extension.sendRequest({cmd: "getData"}, function(response) {
+		text = response.text
+		url = response.url
+	});
+  
+	window.addEventListener("message", function(event){
+	  if(event.data == 'close') {
+  	  J('#close').click()
+	  } else {
+      location.href = "javascript:frames['causatum-frame'].postMessage({url: '"+url+"', text: '"+text+"'}, '*')"
+	  }
+	}, false);
 	
-	var tray = J('<div class="causatum-tray"><div class="causatum-shade"></div><div class="causatum-icons"><div class="causatum-shade" style="left: 40px"></div><p id="causatum-close" class="causatum-icon">close</p></div><div class="causatum-content"><iframe class ="causatum-frame" src="http://www.antiaction.com"></iframe></div></div>')
+	var frame_url =  "http://causatum.org/event/new" //chrome.extension.getURL("frame.html");
+
+	var tray = J('<div class="causatum-tray"><div class="causatum-shade"></div><div class="causatum-icons"><div class="causatum-shade" style="left: 40px"></div><p id="causatum-close" class="causatum-icon">close</p></div><div class="causatum-content"><iframe id="causatum-frame" class ="causatum-frame" src="'+ frame_url +'"></iframe></div></div>')
 	J('body').prepend(tray)
+
 	J('#causatum-close').click(function(){
 		J('.causatum-icons').animate({"left" : "360"},400, function(){
 			J('.causatum-tray').animate({"left" : "-400"},200, function(){
@@ -12,8 +27,6 @@ J(function(){
 			})
 		})
 	})
-
-
 	J('.causatum-tray').animate({"left" : "0"},400, function(){
 		J('.causatum-icons').animate({"left" : "400"},200)
 	})
